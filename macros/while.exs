71eed0a930df0e4d2a0f77c42f3eb6936050ff18
Christophe Belpaire <christophe.belpaire@selinko.com>
2017-05-22 10:02:44 +0200

    Chapter 2, while step 2

--- macros/while.exs
+++ macros/while.exs
@@ -1,12 +1,16 @@
 defmodule Loop do
   defmacro while(expression, do: block) do
     quote do
-      for _ <- Stream.cycle([:ok]) do
-        if unquote(expression) do
-          unquote(block)
-        else
-          # break out of loop
+      try do
+        for _ <- Stream.cycle([:ok]) do
+          if unquote(expression) do
+            unquote(block)
+          else
+            throw :break
+          end
         end
+      catch
+        :break -> :ok
       end
     end
   end

02572caa2604919ccf05304e8fab29d0e0f46cef
Christophe Belpaire <christophe.belpaire@selinko.com>
2017-05-24 13:27:05 +0200

    Chapter 2, Assertion and test mini framework final

--- macros/assertion.exs
+++ macros/assertion.exs
@@ -10,7 +10,7 @@ defmodule Assertion do
   defmacro __before_compile__(_env) do
     quote do
       def run do
-        IO.puts "Running the tests (#{inspect @tests})"
+        Assertion.Test.run(@tests, __MODULE__)
       end
     end
   end
@@ -23,27 +23,46 @@ defmodule Assertion do
     end
   end
 
+  defmacro assert({operator, _, [lhs, rhs]}) do
+    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
+      Assertion.Test.assert(operator, lhs, rhs)
+    end
+  end
+
 end
 
 defmodule Assertion.Test do
+
+  def run(tests, module) do
+    Enum.each tests, fn {test_func, description} ->
+      case apply(module, test_func, []) do
+        :ok             -> IO.write(".")
+        {:fail, reason} -> IO.puts """
+        ==============================
+        FAILURE : #{description}
+        ==============================
+        #{reason}
+        """
+      end
+    end
+  end
+
   def assert(:==, lhs, rhs) when lhs == rhs do
-    IO.write "."
+    :ok
   end
   def assert(:==, lhs, rhs) do
-    IO.puts """
-    FAILURE:
+    {:fail, """
     Expected:        #{lhs}
     to be equals to: #{rhs}
-    """
+    """}
   end
   def assert(:>, lhs, rhs) when lhs > rhs do
-    IO.write "."
+    :ok
   end
   def assert(:>, lhs, rhs) do
-    IO.puts """
-    FAILURE:
+    {:fail, """
     Expected:        #{lhs}
     to be greater to: #{rhs}
-    """
+    """}
   end
 end
--- macros/math_test_final.exs
+++ macros/math_test_final.exs
@@ -2,8 +2,13 @@ defmodule MathTest do
   use Assertion
 
   test "integer can be added and substracted" do
-    assert 1 + 1 == 2
     assert 2 + 3 == 5
     assert 5 - 5 == 10
   end
+
+  test "integer can be multiplied and divided" do
+    assert 5 * 5 == 25
+    assert 10 / 2 == 5
+  end
+
 end

02f7376f0c8bc244fc77d91d4c671ef97e7d4a12
Christophe Belpaire <christophe.belpaire@selinko.com>
2017-06-18 22:05:14 +0200

    Chapter 4, translator tests part 1

---
+++ advanced_code_gen/translator_test.exs
@@ -0,0 +1,40 @@
+ExUnit.start
+Code.require_file("translator.exs", __DIR__)
+
+defmodule TranslatorTest do
+  use ExUnit.Case
+
+  defmodule I18n do
+    use Translator
+    locale "en", [
+      foo: "bar",
+      flash: [
+        notice: [
+          alert: "Alert!",
+          hello: "Hello %{first} %{last}!"
+        ]
+      ],
+      users: [
+        title: "Users",
+        profile: [
+          title: "Profiles"
+        ]
+      ]
+    ]
+    locale "fr", [
+      flash: [
+        notice: [
+          hello: "Salut %{first} %{last}!"
+        ]
+      ]]
+  end
+
+  test "it recursively walks translations tree" do
+    assert I18n.t("en", "users.title") == "Users"
+    assert I18n.t("en", "users.profile.title") == "Profiles"
+  end
+
+  test "it handles translations at root level" do
+    assert I18n.t("en", "foo") == "bar"
+  end
+end

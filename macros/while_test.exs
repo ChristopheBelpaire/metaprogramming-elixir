247a3d7389c449bfee79db84dd620fd1e29e1115
Christophe Belpaire <christophe.belpaire@selinko.com>
2017-06-18 21:42:45 +0200

    Chapter 4, while test part 2

--- macros/while_test.exs
+++ macros/while_test.exs
@@ -8,4 +8,21 @@ defmodule WhileTest do
   test "Is this really taht easy ?" do
     assert Code.ensure_loaded?(Loop)
   end
+
+  test "while/2 loop as long as the expression is truthy" do
+    pid = spawn(fn -> :timer.sleep(:infinity) end)
+
+    send self(), :one
+    while Process.alive?(pid) do
+      receive do
+        :one -> send self(), :two
+        :two -> send self(), :three
+        :three ->
+          Process.exit(pid, :kill)
+          send self(), :done
+      end
+    end
+    assert_received :done
+  end
+
 end

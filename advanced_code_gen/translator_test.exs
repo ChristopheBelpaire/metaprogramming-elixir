3bfd0adf1cc0e3213f74cbc03055c8de1c88ea69
Christophe Belpaire <christophe.belpaire@selinko.com>
2017-06-21 08:48:35 +0200

    Chapter 4, add unit tests to translator

--- advanced_code_gen/translator_test.exs
+++ advanced_code_gen/translator_test.exs
@@ -57,4 +57,34 @@ defmodule TranslatorTest do
   test "convertd interpolations to string" do
     assert I18n.t("en", "flash.notice.hello", first: 123, last: 456) == "Hello 123 456!"
   end
+
+  test "compile/1 generates catch-all t/3 functions" do
+    assert Translator.compile([]) |> Macro.to_string == String.strip ~S"""
+    (
+      def(t(locale, path, bindings \\ []))
+      []
+      def(t(_locale, _path, _bindings)) do
+        {:error, :no_translations}
+      end
+    )
+    """
+  end
+
+  test "compile/1 generates t/3 functions from each locale" do
+    locales = [{"en", [foo: "bar", bar: "%{baz}"]}]
+    assert Translator.compile(locales) |> Macro.to_string == String.strip ~S"""
+    (
+      def(t(locale, path, bindings \\ []))
+      [[def(t("en", "foo", bindings)) do
+        "" <> "bar"
+      end, def(t("en", "bar", bindings)) do
+        ("" <> to_string(Keyword.fetch!(bindings, :baz))) <> ""
+      end]]
+      def(t(_locale, _path, _bindings)) do
+        {:error, :no_translations}
+      end
+    )
+    """
+  end
+
 end
